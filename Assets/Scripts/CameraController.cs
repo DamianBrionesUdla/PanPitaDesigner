90badbd35bf584cf488ca7e78c48bca845debf58
DamianBrionesUdla <damian.briones@udla.edu.ec>
2021-06-30 20:37:41 -0500

    Testpastel
    Se agrego funcionalidad de la camara y el pastel

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    private const int IZQUIERDA = 0;
+    private const int DERECHA = 1;
+    private Transform camaraTransform;
+
+    public float camaraVel = 1;
+    public Transform objetivoTransform;
+    public float distanciaObjetivo = 10;
+
+	void Start()
+	{
+        camaraTransform = GetComponent<Camera>().transform;
+	}
+
+	void Update()
+    {
+        if (Input.GetMouseButton(IZQUIERDA))
+            mover(IZQUIERDA);
+        if (Input.GetMouseButton(DERECHA))
+            mover(DERECHA);
+    }
+
+    private void mover(int dir)
+	{
+        Vector3 vel = dir == DERECHA ? Vector3.right : Vector3.left;
+
+        float rotacionSobreEjeY = vel.x * 180 * Time.deltaTime * camaraVel;
+
+        camaraTransform.position = objetivoTransform.position;
+        camaraTransform.Rotate(new Vector3(0, 1, 0), rotacionSobreEjeY, Space.World);
+
+        camaraTransform.Translate(new Vector3(0, 0.0f, -distanciaObjetivo));
+
+    }
+}
